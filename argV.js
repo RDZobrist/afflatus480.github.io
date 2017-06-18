9832ae772688bdf01c27301d417c2fd623aca359
Robert Zobrist <Robert Zobrist>
2017-06-18 13:17:06 -0700

    added deployed link to star wars rpg

---
+++ argV.js
@@ -0,0 +1,10 @@
+var apple = process.argv[0];
+var orange = process.argv[1];
+var inputString = process.argv.slice(2);
+
+
+if (apple == orange){
+	console.log("true");
+}else{
+	consolle.log("false");
+}
